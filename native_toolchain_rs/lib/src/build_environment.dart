28d204d840b7f5e5799fe9c2cd635d3d0346dde5
Gregory Conrad <gregorysconrad@gmail.com>
2025-10-04 12:22:16 -0400

    fix: drop .exe from NDK binaries and fix NDK target triples

--- native_toolchain_rs/lib/src/build_environment.dart
+++ native_toolchain_rs/lib/src/build_environment.dart
@@ -11,31 +11,6 @@ interface class BuildEnvironmentFactory {
   const BuildEnvironmentFactory();
 
   Map<String, String> createBuildEnvVars(CodeConfig codeConfig) {
-    final CodeConfig(:targetOS, :cCompiler) = codeConfig;
-    final targetTriple = codeConfig.targetTriple;
-    final targetTripleEnvVar = targetTriple.replaceAll('-', '_');
-
-    String getBinary(String binaryName) {
-      if (cCompiler == null) {
-        throw UnsupportedError(
-          'CCompilerConfig was not provided but is required for $targetTriple',
-        );
-      }
-
-      final binaryPath = path.join(
-        path.dirname(path.fromUri(cCompiler.compiler)),
-        OS.current.executableFileName(binaryName),
-      );
-
-      if (!File(binaryPath).existsSync()) {
-        throw RustValidationException([
-          'Binary $binaryPath not found; is your installed compiler too old?',
-        ]);
-      }
-
-      return binaryPath;
-    }
-
     return {
       // NOTE: XCode makes some injections into PATH that break host build
       // for crates with a build.rs
@@ -47,19 +22,65 @@ interface class BuildEnvironmentFactory {
             .join(':'),
       },
 
-      // NOTE: we need to point to NDK >=27 vended LLVM for Android.
-      // The `${targetTriple}35-clang`s were introduced in NDK 27,
-      // so using these binaries:
-      // 1. Ensures we are using a compatible NDK
-      // 2. Also fixes build issues when just using the `clang`s directly
-      if (targetOS == OS.android) ...{
-        'AR_$targetTripleEnvVar': getBinary('llvm-ar'),
-        'CC_$targetTripleEnvVar': getBinary('${targetTriple}35-clang'),
-        'CXX_$targetTripleEnvVar': getBinary('${targetTriple}35-clang++'),
-        'CARGO_TARGET_${targetTripleEnvVar.toUpperCase()}_LINKER': getBinary(
-          '${targetTriple}35-clang',
+      if (codeConfig.targetOS == OS.android)
+        ...const AndroidBuildEnvironmentFactory().createBuildEnvVars(
+          codeConfig: codeConfig,
         ),
-      },
+    };
+  }
+}
+
+@internal
+interface class AndroidBuildEnvironmentFactory {
+  const AndroidBuildEnvironmentFactory();
+
+  Map<String, String> createBuildEnvVars({
+    required CodeConfig codeConfig,
+  }) {
+    final CodeConfig(:targetTriple, :cCompiler) = codeConfig;
+    if (cCompiler == null) {
+      throw UnsupportedError(
+        'CCompilerConfig was not provided but is required for $targetTriple',
+      );
+    }
+
+    String getCompilerPath(String binaryName) {
+      final compilerBinariesDir = path.dirname(
+        path.fromUri(cCompiler.compiler),
+      );
+      final binaryPath = path.join(compilerBinariesDir, binaryName);
+
+      if (!File(binaryPath).existsSync()) {
+        throw RustValidationException([
+          'Binary $binaryPath not found; is your installed NDK too old?',
+        ]);
+      }
+
+      return binaryPath;
+    }
+
+    final targetTripleEnvVar = targetTriple.replaceAll('-', '_');
+    final ndkTargetTriple = switch (targetTriple) {
+      // NOTE: sometimes the Rust and NDK target triples do not match.
+      // See: https://github.com/GregoryConrad/native_toolchain_rs/issues/21#issuecomment-3368307228
+      'armv7-linux-androideabi' => 'armv7a-linux-androideabi',
+      _ => targetTriple,
+    };
+
+    // NOTE: we need to point to NDK >=27 vended LLVM for Android.
+    // The `${ndkTargetTriple}35-clang`s were introduced in NDK 27,
+    // so using these binaries:
+    // 1. Ensures we are using a compatible NDK
+    // 2. Also fixes build issues when just using the `clang`s directly
+    const apiTarget = '35';
+    final clangPath = getCompilerPath('$ndkTargetTriple$apiTarget-clang');
+    final clangPpPath = getCompilerPath('$ndkTargetTriple$apiTarget-clang++');
+
+    return {
+      'AR_$targetTripleEnvVar': path.fromUri(cCompiler.archiver),
+      'CC_$targetTripleEnvVar': clangPath,
+      'CXX_$targetTripleEnvVar': clangPpPath,
+      'CARGO_TARGET_${targetTripleEnvVar.toUpperCase()}_LINKER': clangPath,
     };
   }
 }
